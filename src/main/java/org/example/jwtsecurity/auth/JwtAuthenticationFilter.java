4e2a319b956e0ff460e6c66f4b8d9e2f795732df
hsu-git <hsu080206@gmail.com>
2025-04-23 15:01:36 +0900

    CustomUserDetailsService, Filter

---
+++ src/main/java/org/example/jwtsecurity/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,18 @@
+package org.example.jwtsecurity.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest req,
+                                    HttpServletResponse res,
+                                    FilterChain Chain) throws ServletException, IOException {
+
+    }
+}
